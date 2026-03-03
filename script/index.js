f475cd641b34850e6da817a5a3f6bbb1e58a26f1
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-03-03 17:58:24 +0600

    now add active style on btn

--- script/index.js
+++ script/index.js
@@ -5,22 +5,27 @@ const loadLessons = () => {
     .then(data => displayLesson(data))
 };
 
+const removeActive = () => {
+    const lessonButtons = document.querySelectorAll('.lesson-btn');
+    lessonButtons.forEach(btn => btn.classList.remove('active'));
+};
+
+
 const loadLevelWord = (id) => {
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
     // console.log(url);
     fetch(url)
         .then(res => res.json())
-        .then(data => displayLevelWord(data))
+        .then(data => {
+            removeActive();
+            const clickBtn = document.getElementById(`lesson-btn-${id}`);
+            clickBtn.classList.add('active');
+            // console.log('btn clicked');
+            displayLevelWord(data);
+        });
 
 };
 
-// {
-//     "id": 84,
-//     "level": 1,
-//     "word": "Fish",
-//     "meaning": "মাছ",
-//     "pronunciation": "ফিশ"
-// }
 
 const displayLevelWord = (words) => {
     // console.log(words?.data);
@@ -43,9 +48,9 @@ const displayLevelWord = (words) => {
         const card = document.createElement('div');
         card.innerHTML = `
             <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5">
-            <h2 class="text-3xl font-bold">${word?.word}</h2>
+            <h2 class="text-3xl font-bold">${word?.word ? word?.word : 'Not a meaning'}</h2>
             <p class="text-lg font-medium my-5">Meaning/Pronounciation</p>
-            <div class="text-2xl font-bold font-bangla text-[#18181B]">"${word?.meaning} / ${word?.pronunciation}"</div>
+            <div class="text-xl font-bold font-bangla text-[#18181B]">"${word?.meaning ? word?.meaning : 'Not a meaning'} / ${word?.pronunciation ? word?.pronunciation : 'Not a meaning'}"</div>
             <div class="flex justify-between items-center mt-10">
                 <button class="btn text-lg bg-[#1A91FF10] hover:bg-[#1A91FF90]"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn text-lg bg-[#1A91FF10] hover:bg-[#1A91FF90]"><i class="fa-solid fa-volume-high"></i></button>
@@ -67,7 +72,7 @@ const displayLesson = (lessons) => {
         // 3. create element
         const btnDiv = document.createElement('div');
         btnDiv.innerHTML = `
-            <button onclick="loadLevelWord(${lesson?.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson?.level_no}</button>
+            <button id="lesson-btn-${lesson?.level_no}" onclick="loadLevelWord(${lesson?.level_no})" class="btn btn-outline btn-primary lesson-btn"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson?.level_no}</button>
         `;
 
         // 4. append into container
