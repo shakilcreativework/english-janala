9a9cfbda90a44e447f96572bdb7e2acceb6ed89c
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-03-03 11:54:52 +0600

    Lesson buttons add in UI

---
+++ script/index.js
@@ -0,0 +1,29 @@
+
+const loadLessons = () => {
+    fetch("https://openapi.programming-hero.com/api/levels/all") // promise of response
+    .then(res => res.json()) // promise of json data
+    .then(data => displayLesson(data))
+};
+
+const displayLesson = (lessons) => {
+    // 1. get the container and empty
+    const levelContainer = document.getElementById('level-container');
+    levelContainer.innerHTML = '';
+
+    // 2. get into every lessons
+    for(let lesson of lessons?.data){
+        // 3. create element
+        const btnDiv = document.createElement('div');
+        btnDiv.innerHTML = `
+            <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson?.level_no}</button>
+        `;
+
+        // 4. append into container
+        levelContainer.append(btnDiv);
+        console.log(lesson);
+
+    }
+
+};
+
+loadLessons();
